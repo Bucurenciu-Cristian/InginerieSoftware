6975fe95a1245a6daff660874dc2d8d8edb6d28c
Bucurenciu Bogdan Cristian <bucurenciu.cristian@gmail.com>
2021-11-10 22:40:25 +0200

    Lab 5 is done only now. It works. Added persistence.xml

--- src/main/java/com/park/parkinglot/servlet/Cars.java
+++ src/main/java/com/park/parkinglot/servlet/Cars.java
@@ -58,7 +58,7 @@ public class Cars extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         //        processRequest(request, response);
-        
+
         request.setAttribute("numberOfFreeParkingSpots", 10);
         request.getRequestDispatcher("/WEB-INF/pages/cars.jsp").forward(request, response);
     }

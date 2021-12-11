74cccc2eded48c185620f8cf2487c73f83ac7924
cristian-bucurenciu <bucurenciu.cristian@gmail.com>
2021-12-11 10:43:16 +0200

    Lab 11 is done. Adding Invoices The RESTful isn't working for now. And I didn't change the database to mysql

--- src/main/java/com/park/parkinglot/servlet/Logout.java
+++ src/main/java/com/park/parkinglot/servlet/Logout.java
@@ -31,6 +31,7 @@ public class Logout extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         request.logout();
+        request.getSession().invalidate();
         response.sendRedirect(request.getContextPath());
     }
 
